2ef9a0aa274c9560f26fe33581ebdfd18130fd08
Dennis Hedegaard <dennis@dhedegaard.dk>
2023-03-10 17:01:21 +0100

    formatting + unused variable.

--- static/sudokuweb.js
+++ static/sudokuweb.js
@@ -1,43 +1,42 @@
 /*global $*/
-$(function() {
-    "use strict";
+$(function () {
+  "use strict";
 
-    $('button').click(function(e) {
-        e.preventDefault();
+  $("button").click(function (e) {
+    e.preventDefault();
 
-        /* Assemble the board into a data structure. */
-        var board = []
-        $.each($('[data-x][data-y]'), function(idx, elem) {
-            var $elem = $(elem);
-            var x = $elem.data('x');
-            var y = $elem.data('y');
-            var val = $elem.val();
+    /* Assemble the board into a data structure. */
+    var board = [];
+    $.each($("[data-x][data-y]"), function (idx, elem) {
+      var $elem = $(elem);
+      var x = $elem.data("x");
+      var y = $elem.data("y");
+      var val = $elem.val();
 
-            if (val === '') {
-                val = null;
-            } else {
-                val = parseInt(val);
-            }
+      if (val === "") {
+        val = null;
+      } else {
+        val = parseInt(val);
+      }
 
-            if (x === 0) {
-                board.push([]);
-            }
-            board[y].push(val);
-        });
-
-        /* Make the request. */
-        $.post('solve', {board: JSON.stringify(board)}, function(result) {
-            /* On success, fill out the empty spots and mark them. */
-            $.each(result, function(y, sublist) {
-                $.each(sublist, function(x, elem) {
-                    var input = $('[data-x=' + x + '][data-y=' + y + ']');
-                    if (input.val() === '') {
-                        input.val(result[y][x]);
-                        input.addClass('non-bold');
-                    }
-                });
-            });
-            console.log(resp);
-        })
+      if (x === 0) {
+        board.push([]);
+      }
+      board[y].push(val);
     });
+
+    /* Make the request. */
+    $.post("solve", { board: JSON.stringify(board) }, function (result) {
+      /* On success, fill out the empty spots and mark them. */
+      $.each(result, function (y, sublist) {
+        $.each(sublist, function (x, elem) {
+          var input = $("[data-x=" + x + "][data-y=" + y + "]");
+          if (input.val() === "") {
+            input.val(result[y][x]);
+            input.addClass("non-bold");
+          }
+        });
+      });
+    });
+  });
 });
